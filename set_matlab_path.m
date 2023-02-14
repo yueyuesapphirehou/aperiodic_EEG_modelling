4bdaa3ba0ebdbaebdccdefa39876cad91d1d67a5
Niklas Brake <niklas.brake@mail.mcgill.ca>
2023-02-14 18:27:51 -0500

    add auxiliary functions

---
+++ set_matlab_path.m
@@ -0,0 +1,7 @@
+wd = fileparts(mfilename('fullpath'));
+addpath(fullfile(wd,'_figures'));
+addpath(fullfile(wd,'simulation_examples'));
+addpath(fullfile(wd,'auxiliary'));
+addpath(fullfile(wd,'auxiliary','fmriView'));
+addpath(fullfile(wd,'data_analysis'));
+addpath(fullfile(wd,'simulation_code'));
